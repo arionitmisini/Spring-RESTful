d4b0b34d5ece69b2533a19fba350753d165e2040
arionitmisini <arionitmisini@gmail.com>
2018-06-23 22:34:07 +0200

    Features: Mock Data added and put H2 DB properties

--- src/main/java/com/sample/springresth2/SpringRestH2Application.java
+++ src/main/java/com/sample/springresth2/SpringRestH2Application.java
@@ -1,12 +1,23 @@
 package com.sample.springresth2;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sample.springresth2.models.Person;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringRestH2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestH2Application.class, args);
+
 	}
+
 }
